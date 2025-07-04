87a4a9377646c014d4797ee6bb913701e6136eeb
somanytearsss <somanytearsss@yandex.ru>
2025-07-04 18:16:05 +0300

    Delete base_page_locator.py

--- src/locators/base_page_locator.py
+++
@@ -1,7 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-class BasePageLocators:
-    ACCEPT_COOKIE = By.ID, "rcc-confirm-button"
-    YANDEX_LOGO = By.CLASS_NAME, "Header_LogoYandex__3TSOI"
-    SCOOTER_LOGO = By.XPATH,'//a[@class="Header_LogoScooter__3lsAR"]'
